3fbbb19aa8aada024afa09c5a0908db829ef09d9
marionaka <marionaka@hotmail.com>
2023-06-29 22:04:03 -0300

    first commit

---
+++ src/dao/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    index: true,
+  },
+  password: String,
+  age: Number,
+  img: String,
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
